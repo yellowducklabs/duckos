159cc93f167ab310214e389be4e956290a99aa30
Mario Rogic <hello@mario.net.au>
2014-05-27 20:25:59 +1000

    receive-gzip implementation complete with working test

--- test/x86.sh
+++ test/x86.sh
@@ -4,12 +4,14 @@ set -x
 # @ISSUE This needs to be dynamic, currently hardcoded to the local build VM
 HOST=10.0.1.28
 
+# Remove the host key given host keys change each build
 ssh-keygen -R $HOST
 
-gzip -rc app > app.gz
+
+tar czvf app.tar.gz -C app/ .
 
 # Use private key and skip host checking as keys change each build
 args="-i ../duckauth.priv -o StrictHostKeyChecking=no "
 
-cat app.gz | ssh $args duck@$HOST receive-gzip app
+cat app.tar.gz | ssh $args duck@$HOST receive-gzip app
 
