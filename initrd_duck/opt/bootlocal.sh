e35a7df16fc41bdf11aa75b44be9d81a36877f12
Mario Rogic <hello@mario.net.au>
2014-05-27 21:46:25 +1000

    Leverage .ashrc to deploy app on reboot as a workaround

--- initrd_duck/opt/bootlocal.sh
+++ initrd_duck/opt/bootlocal.sh
@@ -7,9 +7,10 @@ ssh-keygen -A
 sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=0
 
 # Start SSH Daemon
-/usr/local/sbin/sshd
+/usr/local/sbin/sshd &
 
 # Start Quacker
 /home/duck/quacker 10002 &
 
-# duck deploy app
+# Add app deployment to .ashrc
+echo "ducku deploy app" >> /home/duck/.ashrc
