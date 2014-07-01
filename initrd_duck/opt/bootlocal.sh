c3cb9f6e4df5d13b61df179f54491f475a8ebfc4
Mario Rogic <hello@mario.net.au>
2014-07-01 20:30:36 +1000

    Backup wifi.db and attempt to connect to first network on boot

--- initrd_duck/opt/bootlocal.sh
+++ initrd_duck/opt/bootlocal.sh
@@ -9,6 +9,11 @@ fi
 # Respond to broadcasts from duck find
 sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=0 2>&1 > /dev/null
 
+# Autoconnect to the first wifi.db entry via DHCP if we have a db file
+if [ -f /home/duck/wifi.db ]; then
+  wifi.sh -a
+fi
+
 # Start SSH Daemon
 /usr/local/sbin/sshd &
 
